c2852596ddf9883d625ece90554c9997b0923f92
rnadha <rukaan.adha@gmail.com>
2019-03-04 17:35:42 +0700

    Chagne footer

--- components/SnowflakeApp.js
+++ components/SnowflakeApp.js
@@ -203,7 +203,7 @@ class SnowflakeApp extends React.Component<Props, SnowflakeAppState> {
           <div style={{flex: 1}}>
             Made with ❤️ by <a href="https://medium.engineering" target="_blank">Medium Eng</a>.
             Learn about the <a href="https://medium.com/s/engineering-growth-framework" target="_blank">growth framework</a>.
-            Get the <a href="https://github.com/Medium/snowflake" target="_blank">source code</a>.
+            Get the <a href="https://github.com/Medium/snowflake" target="_blank">initial source code</a>.
             Read the <a href="https://medium.com/p/85e078bc15b7" target="_blank">terms of service</a>.
           </div>
         </div>
